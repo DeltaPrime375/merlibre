83cebb351bf498ef0f1c1e97e081ca50c3989274
DeltaPrime375 <juanfcohdez375@gmail.com>
2023-12-12 21:56:18 -0700

    Rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,14 +8,14 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //Rutas Usuarios
-$routes->get('usuarios','UserController::index'); //Obtener productos registrados
-$routes->get('usuarios/new','UserController::new'); //Cargar vista para registrar un nuevo producto
-$routes->post('usuarios','UserController::create'); //Crear un nuevo producto
-$routes->get('usuarios/(:num)','UserController::show/$1'); //Mostrar datos de un producto
-$routes->get('usuarios/edit/(:num)','UserController::edit/$1'); //Cargar vista para modificar datos de un producto
-$routes->put('usuarios/(:num)','UserController::update/$1'); //Modificar los datos de un producto
-$routes->delete('usuarios/(:num)','UserController::delete/$1'); //Eliminar un producto
-$routes->get('usuarios/login','UserController::login'); //Mostrar datos de un producto
+$routes->get('usuarios','UsuariosController::index'); //Obtener productos registrados
+$routes->get('usuarios/new','UsuariosController::new'); //Cargar vista para registrar un nuevo producto
+$routes->post('usuarios','UsuariosController::create'); //Crear un nuevo producto
+$routes->get('usuarios/(:num)','UsuariosController::show/$1'); //Mostrar datos de un producto
+$routes->get('usuarios/edit/(:num)','UsuariosController::edit/$1'); //Cargar vista para modificar datos de un producto
+$routes->put('usuarios/(:num)','UsuariosController::update/$1'); //Modificar los datos de un producto
+$routes->delete('usuarios/(:num)','UsuariosController::delete/$1'); //Eliminar un producto
+$routes->get('usuarios/login','UsuariosController::login'); //Mostrar datos de un producto
 
 //Rutas Productos
 $routes->get('productos','ProductController::index'); //Obtener productos registrados
@@ -30,6 +30,31 @@ $routes->get('productos/search','ProductController::search_index'); //Obtener pr
 //Rutas carrito
 $routes->get('carrito','CartController::index'); //Obtener productos registrados en el carrito por el usuario
 $routes->delete('carrito/(:num)','CartController::delete/$1'); //Eliminar un producto
+
+//Rutas historial
+$routes->get('productos','HistoryController::index'); //Obtener lista de productos comprados por el usuario
+
+// Ventas
+
+$routes->get('compras','VentasController::Index'); // Obtener las compras registradas
+//$routes->get('ventas/new','VentasController::new'); // Cargar la vista para realizar la venta
+//$routes->post('ventas','VentasController::create'); // Crear una nueva venta
+$routes->get('compras/(:num)','VentasController::show/$1');//Mostrar los datos de las compras
+$routes->get('ventas/domicilio/(:num)','VentasController::domicilio/$1');//Mostrar el domicilio del cliente
+$routes->get('ventas/diallega/(:num)','VentasController::diallega/$1');//Mostrar el dia que llega la mercacia
+$routes->get('ventas/contadocredito/(:num)','VentasController::contadocredito/$1');//Selecciona si la compra sera de contado o credito
+$routes->get('ventas/efectivopuntopago/(:num)','VentasController::efectivopuntopago/$1');//Selecciona el punto de pago
+$routes->get('ventas/revisayconfirma/(:num)','VentasController::revisayconfirma/$1');//Confirmar e imprimir el ticket
+
+//Proveedores (Roman)
+
+$routes->get('proveedores','ProveedoresController::index'); //Obtener proveedores registrados
+$routes->get('proveedores/new','ProveedoresController::new'); //Cargar vista para registrar un nuevo proveedor
+$routes->post('proveedores','ProveedoresController::create'); //Crear un nuevo proveedor
+$routes->get('proveedores/(:num)','ProveedoresController::show/$1'); //Mostrar datos de un proveedor
+$routes->get('proveedores/edit/(:num)','ProveedoresController::edit/$1'); //Cargar vista para modificar datos de un proveedor
+$routes->put('proveedores/(:num)','ProveedoresController::update/$1'); //Modificar los datos de un proveedor
+$routes->delete('proveedores/(:num)','ProveedoresController::delete/$1'); //Eliminar un proveedor
 
 //Rutas historial
 $routes->get('productos','HistoryController::index'); //Obtener lista de productos comprados por el usuario
